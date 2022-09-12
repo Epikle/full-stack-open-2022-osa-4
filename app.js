7d92b1d971217bd9e2f9a75d056834056ccb3325
Epikle <98042337+Epikle@users.noreply.github.com>
2022-09-12 14:25:53 +0300

    Added testing for osa5

--- app.js
+++ app.js
@@ -31,6 +31,11 @@ app.use('/api/blogs', blogRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/login', loginRouter);
 
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing');
+  app.use('/api/testing', testingRouter);
+}
+
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
---
+++ controllers/testing.js
@@ -0,0 +1,13 @@
+const testRoutes = require('express').Router();
+
+const Blog = require('../models/blog');
+const User = require('../models/user');
+
+testRoutes.post('/reset', async (req, res) => {
+  await Blog.deleteMany({});
+  await User.deleteMany({});
+
+  res.status(204).end();
+});
+
+module.exports = testRoutes;
